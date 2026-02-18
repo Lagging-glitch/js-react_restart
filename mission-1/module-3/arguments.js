ace9dae8e87c96674791a0efc7d6b2cdb9656335
Jenin <105936413+Lagging-glitch@users.noreply.github.com>
2026-02-18 10:05:48 +0600

    arguments.js in module-3

---
+++ mission-1/module-3/arguments.js
@@ -0,0 +1,14 @@
+/**
+ *arguments: a special array-like object in non-arrow function
+ * not used anymore
+ */
+
+function add(a, b) {
+  // console.log(a + b);
+  // console.log(arguments);
+  // console.log(...arguments);
+  // const params = [...arguments] -->array of spread
+  // console.log(params)
+}
+
+add(23, 27, 25, 30, 5);
