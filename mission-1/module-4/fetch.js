2ff8952e66f9cf36fddf902aade09b5fb82cd072
Jenin <105936413+Lagging-glitch@users.noreply.github.com>
2026-03-06 11:11:16 +0600

    added 'POST' method in fetch.js

--- mission-1/module-4/fetch.js
+++ mission-1/module-4/fetch.js
@@ -3,17 +3,24 @@
  * fetch() gets data as raw HTTP/JSON format
  * .json(): works like JSON.parse(), turns the raw data into workable, js object; it's designed specifically for fetch() responses
  * works with browser console, not node
+ *
  */
 
 // fetch("https://jsonplaceholder.typicode.com/posts/3")
 //  //brings the raw data
+
 // .then((response) => response.json())
 //  //turns the raw data into js objects
+//  // whatever fetch() brings, becomes "response"
+
 // .then((data) => console.log(data))
 //  //gets the actual data
+//  // whatever the 1st .then() brings, becomes the "data"
+
 // .catch((error) => console.log("Error:", error))
 
 //-------------***-----------------//
+// displaying on page
 
 fetch("https://jsonplaceholder.typicode.com/users/1")
   .then((res) => res.json())
@@ -22,3 +29,26 @@ fetch("https://jsonplaceholder.typicode.com/users/1")
     document.getElementById("email").textContent = user.email;
   })
   .catch((error) => console.log("Something went wrong:", error));
+
+//-------------***--------------------//
+//advanced fetch()
+
+fetch("https://jsonplaceholder.typicode.com/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "User 1",
+  }),
+})
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    //   console.log("Success");
+    // } else {
+    //   console.log("Not Successful");
+    }
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
