92e10eee780c8aa5ff349880f49b29e620fb363c
Jenin <105936413+Lagging-glitch@users.noreply.github.com>
2026-03-10 11:17:28 +0600

    added 'lesson-container' in index.html

---
+++ mission-1/module-5(english-janala)/script.js
@@ -0,0 +1,17 @@
+// const displayLessons = (lessons) => {
+//   const lessonContainer = document.getElementById("lesson-container");
+//   lessonContainer.innerHTML = "";
+//   lessons.forEach((lesson) => {
+//     console.log(lesson);
+//   });
+// };
+// const loadLessons = async () => {
+//   const res = await fetch(
+//     "https://openapi.programming-hero.com/api/levels/all/",
+//   );
+//   const lesson = await res.json();
+//   displayLessons(lesson.data);
+// };
+// loadLessons();
+
+//work-in-progress
