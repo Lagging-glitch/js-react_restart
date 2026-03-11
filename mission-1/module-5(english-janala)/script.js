2a2bea9da8d6378a03860e6bdcade47b69d9eab0
Jenin <105936413+Lagging-glitch@users.noreply.github.com>
2026-03-11 10:55:47 +0600

    added loadLessons() & displayLessons() in script.js

--- mission-1/module-5(english-janala)/script.js
+++ mission-1/module-5(english-janala)/script.js
@@ -1,17 +1,35 @@
-// const displayLessons = (lessons) => {
-//   const lessonContainer = document.getElementById("lesson-container");
-//   lessonContainer.innerHTML = "";
-//   lessons.forEach((lesson) => {
-//     console.log(lesson);
-//   });
-// };
-// const loadLessons = async () => {
-//   const res = await fetch(
-//     "https://openapi.programming-hero.com/api/levels/all/",
-//   );
-//   const lesson = await res.json();
-//   displayLessons(lesson.data);
-// };
-// loadLessons();
+const displayLessons = (lessons) => {
+  //lessons is a parameter here
+  const lessonContainer = document.getElementById("lesson-container");
+  lessonContainer.innerHTML = "";
+  lessons.forEach((lesson) => {
+    //lesson is a parameter that refers to each individual elements in "lessons" parameter's place -> passed into displayLessons -> lessons.data -> an array!
+    console.log(lesson);
+    //-----------create div, then paste everything in innerHTML-------------//
+    const btnDiv = document.createElement("div");
 
-//work-in-progress
+    btnDiv.innerHTML = `
+    <button class="btn btn-outline btn-primary">
+      <i class="fa-solid fa-book-open"></i>Lesson ${lesson.level_no}
+      </button>
+    `;
+    lessonContainer.append(btnDiv); // no "" here!
+    //--------classList.add(better)-------//
+    // const lessonBtn = document.createElement("button");
+    // lessonBtn.classList.add("btn",  "btn-outline", "btn-primary");
+    // lessonBtn.innerHTML = `
+    //   <i class="fa-solid fa-book-open"></i>Lesson ${lesson.level_no}
+    // `;
+    // lessonContainer.append(lessonBtn); // works! much cleaner!
+  });
+};
+
+const loadLessons = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/levels/all/",
+  );
+  const lessons = await res.json();
+  // console.log(lessons.data)
+  displayLessons(lessons.data); //lessons.data is a local array, can't be accesed from outside
+};
+loadLessons();
