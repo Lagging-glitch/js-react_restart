f947b30fd12d4d98298bd6215da87a04066b800a
Jenin <105936413+Lagging-glitch@users.noreply.github.com>
2026-03-13 22:34:53 +0600

    adding loadWords() & displayWords() to script.js

--- mission-1/module-5(english-janala)/script.js
+++ mission-1/module-5(english-janala)/script.js
@@ -1,15 +1,17 @@
+// displaying buttons on page function
 const displayLessons = (lessons) => {
   //lessons is a parameter here
-  const lessonContainer = document.getElementById("lesson-container");
-  lessonContainer.innerHTML = "";
+  const lessonContainer = document.getElementById("lesson-container"); //get the container
+  lessonContainer.innerHTML = ""; // empty the container
   lessons.forEach((lesson) => {
+    //call forEach
     //lesson is a parameter that refers to each individual elements in "lessons" parameter's place -> passed into displayLessons -> lessons.data -> an array!
-    console.log(lesson);
+    // console.log(lesson);
     //-----------create div, then paste everything in innerHTML-------------//
     const btnDiv = document.createElement("div");
 
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary">
+    <button class="btn btn-outline btn-primary" onclick = "loadWords(${lesson.level_no})">
       <i class="fa-solid fa-book-open"></i>Lesson ${lesson.level_no}
       </button>
     `;
@@ -24,12 +26,45 @@ const displayLessons = (lessons) => {
   });
 };
 
+//lesson-loading before displaying function
 const loadLessons = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/levels/all/",
-  );
-  const lessons = await res.json();
-  // console.log(lessons.data)
-  displayLessons(lessons.data); //lessons.data is a local array, can't be accesed from outside
+  try {
+    const res = await fetch(
+      "https://openapi.programming-hero.com/api/levels/all/",
+    );
+    const lessons = await res.json();
+    // console.log(lessons.data)
+    displayLessons(lessons.data); //lessons.data is a local array, can't be accesed from outside
+  } catch (error) {
+    console.log("Error:", error);
+  }
 };
 loadLessons();
+
+//lesson-click-load
+const loadWords = async (id) => {
+  try {
+    const res = await fetch(
+      `https://openapi.programming-hero.com/api/level/${id}`,
+    );
+    const getWords = await res.json();
+    console.log(getWords.data);
+    displayWords(getWords.data); //to get the array of objects
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+// lesson-click-display
+const displayWords = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+  const card = document.createElement("div");
+  card.classList.add("shadow-sm", "rounded-lg", "py-4", "px-4", "text-center");
+  card.innerHTML = `
+
+  `;
+  wordContainer.append(card);
+};
+
+loadWords();
